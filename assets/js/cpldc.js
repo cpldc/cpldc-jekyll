aa342b49f693fa4bd344459003611cecd5903e86
N White <nwhite@chipublib.org>
2018-09-10 14:46:34 -0500

    finalizes cards page; does not offer types of search yet

--- assets/js/cpldc.js
+++ assets/js/cpldc.js
@@ -1,12 +1,63 @@
 
-            $( "#hideSidebar" ).click(function() {
-                console.log('clicked to hide');
-                $('#sidebar-toplevel').hide();
-                $('#showSidebar').show();
-                $('.grid').width('100%');
-            });
-            $( "#showSidebar" ).click(function() {
-                console.log('clicked to hide');
-                $('#sidebar-toplevel').show();
-                $('#showSidebar').hide();
-            });
+// when the page is finished loading:
+    // show/hide sidebar buttons are active
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $( "#hideSidebar" ).click(function() {
+        hideSidebar();
+    });
+    $( "#showSidebar" ).click(function() {
+        showSidebar();
+    });
+});
+// sidebar show/hide: 
+    function hideSidebar(){
+        $('#sidebar-toplevel').animate(
+            {width: 'toggle'},
+            {duration: 100,
+                // showing the button before the animation is complete looks jarring
+            complete: function () {
+                    $('#showSidebar').show();
+                }});
+        $('.main-container').animate(
+            {'padding-left': '5px'},
+            {duration: 100});
+    }
+    function showSidebar(){
+        $('#showSidebar').toggle();
+        $('#sidebar-toplevel').animate(
+            {width: 'toggle'},
+            {duration: 100});
+        $('.main-container').animate(
+            {'padding-left': '235px'},
+            {duration: 100});
+    }
+
+    // click search icon to show input field 
+    // when on a collection or subject page it also shows the dropdown to select whether to search all or not
+    function expandSearch(){
+        $('#search-input').show().focus();
+        $('#search-icon').addClass('search-icon-black').removeClass('search-icon-white');
+    }
+    function expandBrowseDropdown(){
+        $('#browseDropdown').toggle();
+    }
+    function searchContent(){
+        var input = $('#search-input').val();
+        // if ( pagetype === "collection" && selectSearchType < 0) {
+        //     var searchUrl = 'http://digital.chipublib.org/digital/collection/' + pagelink + '/search/searchterm/' + input + '/field/all/mode/all/conn/all/order/nosort/ad/asc';
+        // } else if ( pagecolls != '' && selectSearchType < 0) {
+        //     var searchUrl = 'http://digital.chipublib.org/digital/search/collection/' + pagecolls + '/searchterm/' + input + '/field/all/mode/all/conn/all/order/nosort/ad/asc';
+        // } else {
+            var searchUrl = 'http://digital.chipublib.org/digital/search/searchterm/' + input;
+        // }
+        if (input.length > 0 ){
+            window.location.href = searchUrl;
+        }
+    }
+    // media queries: 
+// if page opens at :
+    // sm, md: run hideSidebar()
+// if page shrinks/grows to:
+    // xs, sm, md: run hideSidebar()
+    // lg, xl: 
